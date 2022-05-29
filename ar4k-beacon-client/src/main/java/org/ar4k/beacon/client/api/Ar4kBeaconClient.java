dd415d8bb83f430bab0552ac5fb31c9fbd434672
Andrea Ambrosini - Rossonet s.c.a r.l <andrea.ambrosini@rossonet.org>
2022-05-29 23:11:14 +0200

    aggiunta api client

---
+++ ar4k-beacon-client/src/main/java/org/ar4k/beacon/client/api/Ar4kBeaconClient.java
@@ -0,0 +1,10 @@
+package org.ar4k.beacon.client.api;
+
+import java.io.Serializable;
+import java.util.logging.Logger;
+
+public class Ar4kBeaconClient  {
+
+	private static final Logger logger = Logger.getLogger(Ar4kBeaconClient.class.getName());
+
+}
